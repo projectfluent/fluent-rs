cefdb6cda74cccc1762aea061913365afcb0ee45
Zibi Braniecki <gandalf@mozilla.com>
2017-07-04 12:28:59 -0700

    Fix simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,17 +3,19 @@ extern crate fluent;
 use fluent::context::MessageContext;
 
 fn main() {
-    let mut ctx = MessageContext::new();
+    let mut ctx = MessageContext::new(vec![String::from("und")]);
 
     ctx.add_messages("key1 = Value 1");
     ctx.add_messages("key2 = Value 2");
 
-    match ctx.get_message("key1").and_then(|msg| ctx.format(&msg)) {
+    match ctx.get_message("key1")
+              .and_then(|msg| ctx.format(&msg, None)) {
         Some(value) => println!("{}", value),
         None => println!("None"),
     }
 
-    match ctx.get_message("key2").and_then(|msg| ctx.format(&msg)) {
+    match ctx.get_message("key2")
+              .and_then(|msg| ctx.format(&msg, None)) {
         Some(value) => println!("{}", value),
         None => println!("None"),
     }
