5b77cca9a381296d479ed011ab0c0aa3f27a15c1
Zibi Braniecki <gandalf@mozilla.com>
2017-07-18 23:25:31 -0700

    Add two more tests

--- tests/resolver.rs
+++ tests/resolver.rs
@@ -5,7 +5,7 @@ use self::fluent::context::FluentArgument;
 use std::collections::HashMap;
 
 #[test]
-fn external_argument() {
+fn external_argument_string() {
     let locales = vec![String::from("pl")];
     let mut ctx = MessageContext::new(locales);
 
@@ -21,4 +21,43 @@ fn external_argument() {
     };
 
     assert_eq!(String::from("Hello John"), val);
+}
+
+#[test]
+fn external_argument_number() {
+    let locales = vec![String::from("pl")];
+    let mut ctx = MessageContext::new(locales);
+
+    ctx.add_messages("unread-emails = You have { $emailsCount } unread emails");
+
+    let mut args = HashMap::new();
+    args.insert("emailsCount", FluentArgument::from(5));
+
+    let val = match ctx.get_message("unread-emails")
+              .and_then(|msg| ctx.format(&msg, Some(&args))) {
+        Some(value) => value,
+        None => String::from("None"),
+    };
+
+    assert_eq!(String::from("You have 5 unread emails"), val);
+}
+
+#[test]
+fn reference_message_with_external_argument() {
+    let locales = vec![String::from("pl")];
+    let mut ctx = MessageContext::new(locales);
+
+    ctx.add_messages("greetings = Hello, { $userName }");
+    ctx.add_messages("click-on = Click on the `{ greetings }` label.");
+
+    let mut args = HashMap::new();
+    args.insert("userName", FluentArgument::from("Mary"));
+
+    let val = match ctx.get_message("click-on")
+              .and_then(|msg| ctx.format(&msg, Some(&args))) {
+        Some(value) => value,
+        None => String::from("None"),
+    };
+
+    assert_eq!(String::from("Click on the `Hello, Mary` label."), val);
 }
