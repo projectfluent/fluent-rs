e7194610fa743ca06b3a83bbf217c386fa3ee1af
Zibi Braniecki <gandalf@mozilla.com>
2017-09-15 18:53:22 -0700

    Add very very basic selectors support

---
+++ examples/selector.rs
@@ -0,0 +1,36 @@
+extern crate fluent;
+
+use fluent::context::MessageContext;
+use fluent::context::FluentArgument;
+use std::collections::HashMap;
+
+fn main() {
+    let mut ctx = MessageContext::new("pl");
+
+    ctx.add_messages("
+hello-world = Hello {
+       *[one] World
+        [two] Moon
+    }
+
+hello-world2 = Hello { $name ->
+       *[world] World
+        [moon] Moon
+    }
+");
+
+    match ctx.get_message("hello-world")
+              .and_then(|msg| ctx.format(msg, None)) {
+        Some(value) => println!("{}", value),
+        None => println!("None"),
+    }
+
+    let mut args = HashMap::new();
+    args.insert("name", FluentArgument::from("moon"));
+
+    match ctx.get_message("hello-world2")
+              .and_then(|msg| ctx.format(msg, Some(&args))) {
+        Some(value) => println!("{}", value),
+        None => println!("None"),
+    }
+}
