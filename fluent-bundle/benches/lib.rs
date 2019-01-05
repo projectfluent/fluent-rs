d50fc3eccf804ea18b6ab7e94d482c3afbe851ff
Zibi Braniecki <gandalf@mozilla.com>
2019-01-04 17:31:07 -0800

    Fix benches for fluent-bundle

--- fluent-bundle/benches/lib.rs
+++ fluent-bundle/benches/lib.rs
@@ -3,7 +3,8 @@
 extern crate test;
 
 use fluent_bundle::bundle::FluentBundle;
-use fluent_syntax::{ast, parser::parse};
+use fluent_bundle::resource::FluentResource;
+use fluent_syntax::ast;
 use std::fs::File;
 use std::io;
 use std::io::Read;
@@ -19,11 +20,11 @@ fn read_file(path: &str) -> Result<String, io::Error> {
 #[bench]
 fn bench_simple_format(b: &mut Bencher) {
     let source = read_file("./benches/simple.ftl").expect("Couldn't load file");
-    let resource = parse(&source).unwrap();
+    let res = FluentResource::try_new(source).expect("Couldn't parse an FTL source");
 
     let mut ids = Vec::new();
 
-    for entry in resource.body {
+    for entry in &res.ast().body {
         match entry {
             ast::ResourceEntry::Entry(ast::Entry::Message(ast::Message { id, .. })) => {
                 ids.push(id.name)
@@ -33,7 +34,9 @@ fn bench_simple_format(b: &mut Bencher) {
     }
 
     let mut bundle = FluentBundle::new(&["x-testing"]);
-    bundle.add_messages(&source).unwrap();
+    bundle
+        .add_resource(&res)
+        .expect("Couldn't add FluentResource to the FluentBundle");
 
     b.iter(|| {
         for id in &ids {
@@ -45,11 +48,11 @@ fn bench_simple_format(b: &mut Bencher) {
 #[bench]
 fn bench_menubar_format(b: &mut Bencher) {
     let source = read_file("./benches/menubar.ftl").expect("Couldn't load file");
-    let resource = parse(&source).unwrap();
+    let res = FluentResource::try_new(source).expect("Couldn't parse an FTL source");
 
     let mut ids = Vec::new();
 
-    for entry in resource.body {
+    for entry in &res.ast().body {
         match entry {
             ast::ResourceEntry::Entry(ast::Entry::Message(ast::Message { id, .. })) => {
                 ids.push(id.name)
@@ -59,7 +62,9 @@ fn bench_menubar_format(b: &mut Bencher) {
     }
 
     let mut bundle = FluentBundle::new(&["x-testing"]);
-    bundle.add_messages(&source).unwrap();
+    bundle
+        .add_resource(&res)
+        .expect("Couldn't add FluentResource to the FluentBundle");
 
     b.iter(|| {
         for id in &ids {
