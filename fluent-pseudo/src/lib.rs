664f33cb9aea115f5c448e3bb8345898f7725630
Zibi Braniecki <zibi@braniecki.net>
2019-07-30 19:34:02 -0700

    Update pseudo to support elongation and flipped

--- fluent-pseudo/src/lib.rs
+++ fluent-pseudo/src/lib.rs
@@ -11,19 +11,40 @@ static TRANSFORM_CAPS_MAP: &[char] = &[
     'Ƥ', 'Ɋ', 'Ř', 'Ş', 'Ŧ', 'Ŭ', 'Ṽ', 'Ẇ', 'Ẋ', 'Ẏ', 'Ẑ',
 ];
 
-pub fn transform(s: &str) -> Cow<str> {
+static FLIPPED_SMALL_MAP: &[char] = &[
+    'ɐ', 'q', 'ɔ', 'p', 'ǝ', 'ɟ', 'ƃ', 'ɥ', 'ı', 'ɾ', 'ʞ', 'ʅ', 'ɯ', 'u', 'o', 'd', 'b',
+    'ɹ', 's', 'ʇ', 'n', 'ʌ', 'ʍ', 'x', 'ʎ', 'z',
+];
+static FLIPPED_CAPS_MAP: &[char] = &[
+    '∀', 'Ԑ', 'Ↄ', 'ᗡ', 'Ǝ', 'Ⅎ', '⅁', 'H', 'I', 'ſ', 'Ӽ', '⅂', 'W', 'N', 'O',
+    'Ԁ', 'Ò', 'ᴚ', 'S', '⊥', '∩', 'Ʌ', 'M', 'X', '⅄', 'Z',
+];
+
+pub fn transform(s: &str, flipped: bool, elongate: bool) -> Cow<str> {
     let re_az = Regex::new(r"[a-zA-Z]").unwrap();
+
+    let (small_map, caps_map) = if flipped {
+        (FLIPPED_SMALL_MAP, FLIPPED_CAPS_MAP)
+    } else {
+        (TRANSFORM_SMALL_MAP, TRANSFORM_CAPS_MAP)
+    };
 
     re_az.replace_all(s, |caps: &Captures| {
         let ch = caps[0].chars().nth(0).unwrap();
         let cc = ch as u8;
         if cc >= 97 && cc <= 122 {
             let pos = cc - 97;
-            let new_char = TRANSFORM_SMALL_MAP[pos as usize];
-            new_char.to_string()
+            let new_char = small_map[pos as usize];
+            if elongate && (cc == 97 || cc == 101 || cc == 111 || cc == 117) {
+                let mut s = new_char.to_string();
+                s.push(new_char);
+                s
+            } else {
+                new_char.to_string()
+            }
         } else if cc >= 65 && cc <= 90 {
             let pos = cc - 65;
-            let new_char = TRANSFORM_CAPS_MAP[pos as usize];
+            let new_char = caps_map[pos as usize];
             new_char.to_string()
         } else {
             ch.to_string()
@@ -37,7 +58,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let x = transform("Hello World");
+        let x = transform("Hello World", false, true);
+        assert_eq!(x, "Ħḗḗŀŀǿǿ Ẇǿǿřŀḓ");
+
+        let x = transform("Hello World", false, false);
         assert_eq!(x, "Ħḗŀŀǿ Ẇǿřŀḓ");
+
+        let x = transform("Hello World", true, false);
+        assert_eq!(x, "Hǝʅʅo Moɹʅp");
     }
 }
