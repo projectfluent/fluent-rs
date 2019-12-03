342b196f7c38c2d7efb3338cf077ea2113835b30
Zibi Braniecki <zibi@braniecki.net>
2019-12-02 20:18:36 -0800

    Reorganize lexer

--- fluent-syntax/src/bin/lexer.rs
+++ fluent-syntax/src/bin/lexer.rs
@@ -2,7 +2,7 @@ use fluent_syntax::parser::lexer;
 use std::fmt::Write;
 
 fn main() {
-    let input = include_str!("../../benches/simple.ftl");
+    let input = include_str!("../../benches/menubar.ftl");
 
     let lexer = lexer::Lexer::new(input.as_bytes());
     let tokens: Vec<lexer::Token> = lexer.collect();
--- fluent-syntax/src/parser/lexer.rs
+++ fluent-syntax/src/parser/lexer.rs
@@ -17,8 +17,16 @@ pub enum Token {
 #[derive(Debug, PartialEq)]
 pub enum LexerState {
     Resource,
-    Text,
-    TextLine,
+    Message, // Or Term
+    Pattern,
+    Comment,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum NextLine {
+    TextContinuation,
+    Attribute,
+    NewEntry,
 }
 
 #[derive(Debug)]
@@ -27,6 +35,7 @@ pub struct Lexer<'l> {
     pub source: &'l [u8],
     pub length: usize,
     pub ptr: usize,
+    pub buffer: Option<Token>,
 }
 
 impl<'l> Lexer<'l> {
@@ -37,6 +46,7 @@ impl<'l> Lexer<'l> {
             source,
             length,
             ptr: 0,
+            buffer: None
         }
     }
 
@@ -52,28 +62,54 @@ impl<'l> Lexer<'l> {
         Token::Identifier(start..self.ptr)
     }
 
-    fn tokenize_resource(&mut self) -> Option<Token> {
+    fn tokenize_message(&mut self) -> Option<Token> {
+        let start = self.ptr;
         while let Some(cc) = self.source.get(self.ptr) {
             if *cc != b' ' {
                 break;
             }
             self.ptr += 1;
         }
+        let indent = self.ptr - start;
 
         match self.source.get(self.ptr) {
-            Some(cc) if cc.is_ascii_alphabetic() => Some(self.get_ident()),
             Some(b'=') => {
-                self.state = LexerState::Text;
+                self.state = LexerState::Pattern;
                 self.ptr += 1;
                 Some(Token::EqSign)
-            }
+            },
+            Some(b'.') if indent > 0 => {
+                self.ptr += 1;
+                Some(Token::Dot)
+            },
+            Some(cc) if cc.is_ascii_alphabetic() => {
+                Some(self.get_ident())
+            },
+            None => None,
+            _ => {
+                println!("{:#}", self.source[self.ptr]);
+                panic!()
+            },
+        }
+    }
+
+    fn tokenize_resource(&mut self) -> Option<Token> {
+        match self.source.get(self.ptr) {
+            Some(cc) if cc.is_ascii_alphabetic() => {
+                self.state = LexerState::Message;
+                Some(self.get_ident())
+            },
+            Some(b'-') => {
+                self.ptr += 1;
+                Some(Token::MinusSign)
+            },
             Some(b'#') => {
                 let start = self.ptr;
                 self.ptr += 1;
                 while let Some(b'#') = self.source.get(self.ptr) {
                     self.ptr += 1;
                 }
-                self.state = LexerState::TextLine;
+                self.state = LexerState::Comment;
                 match self.ptr - start {
                     1 => Some(Token::CommentSign),
                     2 => Some(Token::GroupCommentSign),
@@ -85,23 +121,12 @@ impl<'l> Lexer<'l> {
                 self.ptr += 1;
                 Some(Token::Eol)
             }
-            Some(b'.') => {
-                self.ptr += 1;
-                Some(Token::Dot)
-            }
-            Some(b'-') => {
-                self.ptr += 1;
-                Some(Token::MinusSign)
-            }
             None => None,
-            _ => {
-                println!("{:#?}", self.source[self.ptr]);
-                panic!()
-            }
+            _ => panic!()
         }
     }
 
-    fn tokenize_text_line(&mut self) -> Option<Token> {
+    fn tokenize_comment(&mut self) -> Option<Token> {
         let indent_start = self.ptr;
 
         while let Some(cc) = self.source.get(self.ptr) {
@@ -125,28 +150,38 @@ impl<'l> Lexer<'l> {
         Some(Token::Text(indent, start..self.ptr - 1))
     }
 
-    fn tokenize_text(&mut self) -> Option<Token> {
+    fn tokenize_pattern(&mut self) -> Option<Token> {
         let indent_start = self.ptr;
+        let mut in_indent = true;
+        let mut start = self.ptr;
+        let mut indent = 0;
 
         while let Some(cc) = self.source.get(self.ptr) {
-            if *cc != b' ' {
-                break;
+            if in_indent {
+                if *cc == b' ' {
+                    self.ptr += 1;
+                    continue;
+                } else {
+                    start = self.ptr;
+                    indent = start - indent_start;
+                    in_indent = false;
+                }
             }
-            self.ptr += 1;
-        }
-
-        let start = self.ptr;
-        let indent = start - indent_start;
-
-        while let Some(cc) = self.source.get(self.ptr) {
             match cc {
                 b'\n' if start == self.ptr => {
                     self.ptr += 1;
-                    if self.try_if_line_is_text() {
-                        return Some(Token::Eol);
-                    } else {
-                        self.state = LexerState::Resource;
-                        return Some(Token::Eot);
+                    match self.check_next_line() {
+                        NextLine::TextContinuation => return Some(Token::Eol),
+                        NextLine::Attribute => {
+                            self.state = LexerState::Message;
+                            self.buffer = Some(Token::Dot);
+                            self.ptr += 1;
+                            return Some(Token::Eot);
+                        },
+                        NextLine::NewEntry => {
+                            self.state = LexerState::Resource;
+                            return Some(Token::Eot);
+                        }
                     }
                 }
                 b'\n' => {
@@ -159,7 +194,7 @@ impl<'l> Lexer<'l> {
         None
     }
 
-    fn try_if_line_is_text(&mut self) -> bool {
+    fn check_next_line(&mut self) -> NextLine {
         let indent_start = self.ptr;
 
         while let Some(cc) = self.source.get(self.ptr) {
@@ -173,15 +208,14 @@ impl<'l> Lexer<'l> {
         let indent = start - indent_start;
 
         if indent == 0 {
-            return false;
+            return NextLine::NewEntry;
         }
 
         match self.source.get(self.ptr) {
             Some(b'.') => {
-                self.ptr -= 1;
-                false
+                NextLine::Attribute
             }
-            _ => true,
+            _ => NextLine::TextContinuation,
         }
     }
 }
@@ -190,10 +224,15 @@ impl<'l> Iterator for Lexer<'l> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.state {
-            LexerState::Resource => self.tokenize_resource(),
-            LexerState::Text => self.tokenize_text(),
-            LexerState::TextLine => self.tokenize_text_line(),
+        if let Some(token) = self.buffer.take() {
+            return Some(token);
         }
+        let x = match self.state {
+            LexerState::Resource => self.tokenize_resource(),
+            LexerState::Message => self.tokenize_message(),
+            LexerState::Pattern => self.tokenize_pattern(),
+            LexerState::Comment => self.tokenize_comment(),
+        };
+        x
     }
 }
