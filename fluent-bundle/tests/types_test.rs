1d779af58048e17c063ba3ada818823e7da96b80
Zibi Braniecki <zibi@braniecki.net>
2020-01-29 01:01:19 +0100

    Add number related tests

--- fluent-bundle/tests/types_test.rs
+++ fluent-bundle/tests/types_test.rs
@@ -1,7 +1,12 @@
 use fluent_bundle::resolve::Scope;
+use fluent_bundle::types::{
+    FluentNumber, FluentNumberCurrencyDisplayStyle, FluentNumberOptions, FluentNumberStyle,
+};
+use fluent_bundle::FluentArgs;
 use fluent_bundle::FluentBundle;
 use fluent_bundle::FluentResource;
 use fluent_bundle::FluentValue;
+use intl_pluralrules::operands::PluralOperands;
 use unic_langid::langid;
 
 #[test]
@@ -73,4 +78,79 @@ fn fluent_value_from() {
 
     assert_eq!(value_f64, FluentValue::from(23.5));
     assert_eq!(value_isize, FluentValue::from(-23));
+}
+
+#[test]
+fn fluent_number_style() {
+    let fns_decimal: FluentNumberStyle = "decimal".into();
+    let fns_currency: FluentNumberStyle = "currency".into();
+    let fns_percent: FluentNumberStyle = "percent".into();
+    let fns_decimal2: FluentNumberStyle = "other".into();
+    assert_eq!(fns_decimal, FluentNumberStyle::Decimal);
+    assert_eq!(fns_currency, FluentNumberStyle::Currency);
+    assert_eq!(fns_percent, FluentNumberStyle::Percent);
+    assert_eq!(fns_decimal2, FluentNumberStyle::Decimal);
+
+    let fncds_symbol: FluentNumberCurrencyDisplayStyle = "symbol".into();
+    let fncds_code: FluentNumberCurrencyDisplayStyle = "code".into();
+    let fncds_name: FluentNumberCurrencyDisplayStyle = "name".into();
+    let fncds_symbol2: FluentNumberCurrencyDisplayStyle = "other".into();
+
+    assert_eq!(fncds_symbol, FluentNumberCurrencyDisplayStyle::Symbol);
+    assert_eq!(fncds_code, FluentNumberCurrencyDisplayStyle::Code);
+    assert_eq!(fncds_name, FluentNumberCurrencyDisplayStyle::Name);
+    assert_eq!(fncds_symbol2, FluentNumberCurrencyDisplayStyle::Symbol);
+
+    let mut fno = FluentNumberOptions::default();
+
+    let mut args = FluentArgs::new();
+    args.insert("style", "currency".into());
+    args.insert("currency", "EUR".into());
+    args.insert("currencyDisplay", "code".into());
+    args.insert("useGrouping", "true".into());
+    args.insert("minimumIntegerDigits", 3.into());
+    args.insert("minimumFractionDigits", 3.into());
+    args.insert("maximumFractionDigits", 8.into());
+    args.insert("minimumSignificantDigits", 1.into());
+    args.insert("maximumSignificantDigits", 10.into());
+    args.insert("someRandomOption", 10.into());
+
+    fno.merge(&args);
+
+    assert_eq!(fno.style, FluentNumberStyle::Currency);
+    assert_eq!(fno.currency, Some("EUR".to_string()));
+    assert_eq!(fno.currency_display, FluentNumberCurrencyDisplayStyle::Code);
+    assert_eq!(fno.use_grouping, true);
+
+    let num = FluentNumber::new(0.2, FluentNumberOptions::default());
+    assert_eq!(num.as_string(), "0.2");
+
+    let opts = FluentNumberOptions {
+        minimum_fraction_digits: Some(3),
+        ..Default::default()
+    };
+
+    let num = FluentNumber::new(0.2, opts.clone());
+    assert_eq!(num.as_string(), "0.200");
+
+    let num = FluentNumber::new(2.0, opts.clone());
+    assert_eq!(num.as_string(), "2.000");
+}
+
+#[test]
+fn fluent_number_to_operands() {
+    let num = FluentNumber::new(2.81, FluentNumberOptions::default());
+    let operands: PluralOperands = (&num).into();
+
+    assert_eq!(
+        operands,
+        PluralOperands {
+            n: 2.81,
+            i: 2,
+            v: 2,
+            w: 2,
+            f: 81,
+            t: 81,
+        }
+    );
 }
