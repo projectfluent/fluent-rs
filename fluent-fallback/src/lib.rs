ea99f5ba4d1f0c19539fc54cc8c2c55b927933c6
Zibi Braniecki <zibi@braniecki.net>
2020-03-16 13:01:56 -0700

    Switch FluentBundleIterator to be generic. (#166)

--- fluent-fallback/src/lib.rs
+++ fluent-fallback/src/lib.rs
@@ -6,37 +6,48 @@ use fluent_bundle::{FluentArgs, FluentBundle};
 
 use reiterate::Reiterate;
 
-struct FluentBundleIterator<'loc, R> {
-    iter: Box<dyn Iterator<Item = FluentBundle<R>> + 'loc>,
+struct FluentBundleIterator<R, I>
+where
+    I: Iterator<Item = FluentBundle<R>>,
+{
+    iter: I,
 }
 
-impl<'loc, R> Iterator for FluentBundleIterator<'loc, R> {
+impl<R, I> Iterator for FluentBundleIterator<R, I>
+where
+    I: Iterator<Item = FluentBundle<R>>,
+{
     type Item = Box<FluentBundle<R>>;
     fn next(&mut self) -> Option<Self::Item> {
         self.iter.next().map(Box::new)
     }
 }
 
-pub struct Localization<'loc, R> {
+pub struct Localization<'loc, R, I>
+where
+    I: Iterator<Item = FluentBundle<R>> + 'loc,
+{
     pub resource_ids: Vec<String>,
-    bundles: Reiterate<FluentBundleIterator<'loc, R>>,
-    generate_bundles: Box<dyn FnMut(&[String]) -> FluentBundleIterator<'loc, R> + 'loc>,
+    bundles: Reiterate<FluentBundleIterator<R, I>>,
+    generate_bundles: Box<dyn FnMut(&[String]) -> FluentBundleIterator<R, I> + 'loc>,
 }
 
-impl<'loc, R> Localization<'loc, R> {
-    pub fn new<F, I>(resource_ids: Vec<String>, mut generate_bundles: F) -> Self
+impl<'loc, R, I> Localization<'loc, R, I>
+where
+    I: Iterator<Item = FluentBundle<R>>,
+{
+    pub fn new<F>(resource_ids: Vec<String>, mut generate_bundles: F) -> Self
     where
         F: FnMut(&[String]) -> I + 'loc,
-        I: Iterator<Item = FluentBundle<R>> + 'loc,
     {
-        let mut generate2 = move |x: &[String]| FluentBundleIterator {
-            iter: Box::new(generate_bundles(x)),
+        let mut generate = move |x: &[String]| FluentBundleIterator {
+            iter: generate_bundles(x),
         };
-        let bundles = Reiterate::new(generate2(&resource_ids));
+        let bundles = Reiterate::new(generate(&resource_ids));
         Localization {
             resource_ids,
             bundles,
-            generate_bundles: Box::new(generate2),
+            generate_bundles: Box::new(generate),
         }
     }
 
