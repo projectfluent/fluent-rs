fc549cb1d7d110d9e24ea23818c236c7579ed408
Zibi Braniecki <zibi@braniecki.net>
2020-08-27 17:48:07 -0700

    Try to add Stream

--- fluent-fallback/src/lib.rs
+++ fluent-fallback/src/lib.rs
@@ -2,72 +2,88 @@ use std::borrow::Borrow;
 use std::borrow::Cow;
 use std::path::PathBuf;
 
+use futures::stream::{Stream, StreamExt};
+
 use fluent_bundle::FluentResource;
 use fluent_bundle::{FluentArgs, FluentBundle};
 
-use reiterate::Reiterate;
+pub struct Localization {}
 
-struct FluentBundleIteratorSync<R, I>
-where
-    I: Iterator<Item = FluentBundle<R>>,
-{
-    iter: I,
-}
-
-impl<R, I> Iterator for FluentBundleIteratorSync<R, I>
-where
-    I: Iterator<Item = FluentBundle<R>>,
-{
-    type Item = Box<FluentBundle<R>>;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(Box::new)
-    }
-}
-
-pub struct Localization<'loc, R, I>
-where
-    I: Iterator<Item = FluentBundle<R>> + 'loc,
-{
-    pub resource_ids: Vec<PathBuf>,
-    bundles: Reiterate<FluentBundleIteratorSync<R, I>>,
-    generate_bundles: Box<dyn FnMut(Vec<PathBuf>) -> FluentBundleIteratorSync<R, I> + 'loc>,
-}
-
-impl<'loc, R, I> Localization<'loc, R, I>
-where
-    I: Iterator<Item = FluentBundle<R>>,
-{
-    pub fn new<F>(resource_ids: Vec<PathBuf>, mut generate_bundles: F) -> Self
-    where
-        F: FnMut(Vec<PathBuf>) -> I + 'loc,
-    {
-        let mut generate = move |x: Vec<PathBuf>| FluentBundleIteratorSync {
-            iter: generate_bundles(x),
-        };
-        let bundles = Reiterate::new(generate(resource_ids.clone()));
-        Localization {
-            resource_ids,
-            bundles,
-            generate_bundles: Box::new(generate),
+impl Localization {
+    pub fn new<I, R>(
+        resource_ids: Vec<PathBuf>,
+        // generate_bundles: Option<impl FnMut(Vec<PathBuf>) -> S>,
+        generate_bundles_sync: Option<impl FnMut(&[PathBuf]) -> I>,
+    ) -> Self
+        where
+            I: Iterator<Item = FluentBundle<R>>,
+            // S: Stream<Item = FluentBundle<R>>
+        {
+        Self {
+            // resource_ids,
+            // generate_bundles,
+            // generate_bundles_sync,
         }
     }
+// pub struct Localization<R, I, S>
+// where
+//     I: Iterator<Item = FluentBundle<R>>,
+//     S: Stream<Item = FluentBundle<R>>
+// {
+//     resource_ids: Vec<PathBuf>,
+//     generate_bundles: Option<fn(Vec<PathBuf>) -> S>,
+//     generate_bundles_sync: Option<fn(Vec<PathBuf>) -> I>,
+// }
+//
+// impl<'l, R: 'l, I, S> Localization<R, I, S>
+// where
+//     I: Iterator<Item = FluentBundle<R>>,
+//     S: Stream<Item = FluentBundle<R>>,
+// {
+//     pub fn new<F, A>(
+//         resource_ids: Vec<PathBuf>,
+//         generate_bundles: Option<fn(Vec<PathBuf>) -> S>,
+//         generate_bundles_sync: Option<fn(Vec<PathBuf>) -> I>,
+//     ) -> Self {
+//         Self {
+//             resource_ids,
+//             generate_bundles,
+//             generate_bundles_sync,
+//         }
+//     }
 
-    pub fn on_change(&mut self) {
-        self.bundles = Reiterate::new((self.generate_bundles)(self.resource_ids.clone()));
-    }
-
-    pub fn format_value<'l>(&'l self, id: &'l str, args: Option<&'l FluentArgs>) -> Cow<'l, str>
-    where
-        R: Borrow<FluentResource>,
-    {
-        for bundle in &self.bundles {
-            if let Some(msg) = bundle.get_message(id) {
-                if let Some(pattern) = msg.value {
-                    let mut errors = vec![];
-                    return bundle.format_pattern(pattern, args, &mut errors);
-                }
-            }
-        }
-        id.into()
-    }
+    // pub async fn format_value(&mut self, id: &'l str, args: Option<&'l FluentArgs<'_>>) -> Cow<'l, str>
+    // where
+    //     R: Borrow<FluentResource>,
+    // {
+    //     let bundles = self.generate_bundles.unwrap()(self.resource_ids.clone());
+    //     let mut i = Box::pin(bundles);
+    //     while let Some(bundle) = (i.next()).await {
+    //         if let Some(msg) = bundle.get_message(id) {
+    //             if let Some(pattern) = msg.value {
+    //                 let mut errors = vec![];
+    //                 let val: Cow<'_, str> = bundle.format_pattern(pattern, args, &mut errors);
+    //                 return val.to_string().into();
+    //             }
+    //         }
+    //     }
+    //     "Missing".into()
+    // }
+    //
+    // pub fn format_value_sync(&mut self, id: &'l str, args: Option<&'l FluentArgs>) -> Cow<'l, str>
+    // where
+    //     R: Borrow<FluentResource>,
+    // {
+    //     let bundles = self.generate_bundles_sync.unwrap()(self.resource_ids.clone());
+    //     for bundle in bundles {
+    //         if let Some(msg) = bundle.get_message(id) {
+    //             if let Some(pattern) = msg.value {
+    //                 let mut errors = vec![];
+    //                 let val: Cow<'_, str> = bundle.format_pattern(pattern, args, &mut errors);
+    //                 return val.to_string().into();
+    //             }
+    //         }
+    //     }
+    //     "Missing".into()
+    // }
 }
--- fluent-fallback/tests/localization_test.rs
+++ fluent-fallback/tests/localization_test.rs
@@ -1,106 +1,106 @@
-use elsa::FrozenMap;
-use fluent_bundle::FluentBundle;
-use fluent_bundle::FluentResource;
-use fluent_fallback::Localization;
-use unic_langid::langid;
+// use elsa::FrozenMap;
+// use fluent_bundle::FluentBundle;
+// use fluent_bundle::FluentResource;
+// use fluent_fallback::Localization;
+// use unic_langid::langid;
+//
+// use std::cell::RefCell;
+// use std::fs;
+// use std::io;
+// use std::iter;
+// use std::path::{Path, PathBuf};
+//
+// fn read_file(path: &str) -> Result<String, io::Error> {
+//     fs::read_to_string(path)
+// }
+//
+// #[test]
+// fn localization_format() {
+//     let resources: FrozenMap<String, Box<FluentResource>> = FrozenMap::new();
+//
+//     let resource_ids: Vec<PathBuf> = vec!["test.ftl".into(), "test2.ftl".into()];
+//     let res_path_scheme = "./tests/resources/{locale}/{res_id}";
+//     let locales = vec![langid!("pl"), langid!("en-US")];
+//
+//     let res_ids: Vec<&Path> = resource_ids.iter().map(|res_id| res_id.as_path()).collect();
+//     let generate_messages = |_res_ids: Vec<PathBuf>| {
+//         let mut locales = locales.iter();
+//         let res_mgr = &resources;
+//         let res_ids = res_ids.to_vec();
+//
+//         iter::from_fn(move || {
+//             locales.next().map(|locale| {
+//                 let mut bundle = FluentBundle::new(vec![locale]);
+//                 let res_path = res_path_scheme.replace("{locale}", &locale.to_string());
+//
+//                 for res_id in &res_ids {
+//                     let path = res_path.replace("{res_id}", res_id.to_str().unwrap());
+//                     let res = res_mgr.get(&path).unwrap_or_else(|| {
+//                         let source = read_file(&path).unwrap();
+//                         let res = FluentResource::try_new(source).unwrap();
+//                         res_mgr.insert(path, Box::new(res))
+//                     });
+//                     bundle.add_resource(res).unwrap();
+//                 }
+//                 bundle
+//             })
+//         })
+//     };
+//
+//     let loc = Localization::new(resource_ids.clone(), None, Some(generate_messages));
+//
+//     let value = loc.format_value_sync("hello-world", None);
+//     assert_eq!(value, "Hello World [pl]");
+//
+//     let value = loc.format_value_sync("missing-message", None);
+//     assert_eq!(value, "missing-message");
+//
+//     let value = loc.format_value_sync("hello-world-3", None);
+//     assert_eq!(value, "Hello World 3 [en]");
+// }
 
-use std::cell::RefCell;
-use std::fs;
-use std::io;
-use std::iter;
-use std::path::{Path, PathBuf};
-
-fn read_file(path: &str) -> Result<String, io::Error> {
-    fs::read_to_string(path)
-}
-
-#[test]
-fn localization_format() {
-    let resources: FrozenMap<String, Box<FluentResource>> = FrozenMap::new();
-
-    let resource_ids: Vec<PathBuf> = vec!["test.ftl".into(), "test2.ftl".into()];
-    let res_path_scheme = "./tests/resources/{locale}/{res_id}";
-    let locales = vec![langid!("pl"), langid!("en-US")];
-
-    let res_ids: Vec<&Path> = resource_ids.iter().map(|res_id| res_id.as_path()).collect();
-    let generate_messages = |_res_ids: Vec<PathBuf>| {
-        let mut locales = locales.iter();
-        let res_mgr = &resources;
-        let res_ids = res_ids.to_vec();
-
-        iter::from_fn(move || {
-            locales.next().map(|locale| {
-                let mut bundle = FluentBundle::new(vec![locale]);
-                let res_path = res_path_scheme.replace("{locale}", &locale.to_string());
-
-                for res_id in &res_ids {
-                    let path = res_path.replace("{res_id}", res_id.to_str().unwrap());
-                    let res = res_mgr.get(&path).unwrap_or_else(|| {
-                        let source = read_file(&path).unwrap();
-                        let res = FluentResource::try_new(source).unwrap();
-                        res_mgr.insert(path, Box::new(res))
-                    });
-                    bundle.add_resource(res).unwrap();
-                }
-                bundle
-            })
-        })
-    };
-
-    let loc = Localization::new(resource_ids.clone(), generate_messages);
-
-    let value = loc.format_value("hello-world", None);
-    assert_eq!(value, "Hello World [pl]");
-
-    let value = loc.format_value("missing-message", None);
-    assert_eq!(value, "missing-message");
-
-    let value = loc.format_value("hello-world-3", None);
-    assert_eq!(value, "Hello World 3 [en]");
-}
-
-#[test]
-fn localization_on_change() {
-    let resources: FrozenMap<String, Box<FluentResource>> = FrozenMap::new();
-
-    let resource_ids: Vec<PathBuf> = vec!["test.ftl".into(), "test2.ftl".into()];
-    let res_path_scheme = "./tests/resources/{locale}/{res_id}";
-
-    let available_locales = RefCell::new(vec![langid!("en-US")]);
-
-    let res_ids: Vec<&Path> = resource_ids.iter().map(|res_id| res_id.as_path()).collect();
-    let generate_messages = |_res_ids: Vec<PathBuf>| {
-        let mut bundles = vec![];
-
-        for locale in available_locales.borrow().iter() {
-            let mut bundle = FluentBundle::new(vec![locale]);
-            let res_path = res_path_scheme.replace("{locale}", &locale.to_string());
-            for res_id in &res_ids {
-                let path = res_path.replace("{res_id}", res_id.to_str().unwrap());
-                let res = if let Some(res) = resources.get(&path) {
-                    res
-                } else {
-                    let source = read_file(&path).unwrap();
-                    let res = FluentResource::try_new(source).unwrap();
-                    resources.insert(path, Box::new(res))
-                };
-                bundle.add_resource(res).unwrap();
-            }
-            bundles.push(bundle);
-        }
-
-        return bundles.into_iter();
-    };
-
-    let mut loc = Localization::new(resource_ids.clone(), generate_messages);
-
-    let value = loc.format_value("hello-world", None);
-    assert_eq!(value, "Hello World [en]");
-
-    available_locales.borrow_mut().insert(0, langid!("pl"));
-
-    loc.on_change();
-
-    let value = loc.format_value("hello-world", None);
-    assert_eq!(value, "Hello World [pl]");
-}
+// #[test]
+// fn localization_on_change() {
+//     let resources: FrozenMap<String, Box<FluentResource>> = FrozenMap::new();
+//
+//     let resource_ids: Vec<PathBuf> = vec!["test.ftl".into(), "test2.ftl".into()];
+//     let res_path_scheme = "./tests/resources/{locale}/{res_id}";
+//
+//     let available_locales = RefCell::new(vec![langid!("en-US")]);
+//
+//     let res_ids: Vec<&Path> = resource_ids.iter().map(|res_id| res_id.as_path()).collect();
+//     let generate_messages = |_res_ids: Vec<PathBuf>| {
+//         let mut bundles = vec![];
+//
+//         for locale in available_locales.borrow().iter() {
+//             let mut bundle = FluentBundle::new(vec![locale]);
+//             let res_path = res_path_scheme.replace("{locale}", &locale.to_string());
+//             for res_id in &res_ids {
+//                 let path = res_path.replace("{res_id}", res_id.to_str().unwrap());
+//                 let res = if let Some(res) = resources.get(&path) {
+//                     res
+//                 } else {
+//                     let source = read_file(&path).unwrap();
+//                     let res = FluentResource::try_new(source).unwrap();
+//                     resources.insert(path, Box::new(res))
+//                 };
+//                 bundle.add_resource(res).unwrap();
+//             }
+//             bundles.push(bundle);
+//         }
+//
+//         return bundles.into_iter();
+//     };
+//
+//     let mut loc = Localization::new(resource_ids.clone(), generate_messages);
+//
+//     let value = loc.format_value("hello-world", None);
+//     assert_eq!(value, "Hello World [en]");
+//
+//     available_locales.borrow_mut().insert(0, langid!("pl"));
+//
+//     loc.on_change();
+//
+//     let value = loc.format_value("hello-world", None);
+//     assert_eq!(value, "Hello World [pl]");
+// }
