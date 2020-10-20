37a71277497d5d822d1b02860c2a318ec78611c1
Dan Glastonbury <dan.glastonbury@gmail.com>
2020-10-20 14:26:41 +1000

    Fix l10nregistry tests.

--- fluent-fallback/tests/l10nregistry.rs
+++ fluent-fallback/tests/l10nregistry.rs
@@ -1,32 +1,24 @@
-use fluent_bundle::FluentResource;
-use fluent_fallback::{L10nKey, Localization};
-use l10nregistry::registry::L10nRegistry;
-use l10nregistry::source::FileSource;
 use std::borrow::Cow;
-use std::rc::Rc;
+
+use fluent_fallback::{AsyncLocalization, L10nKey, SyncLocalization};
+use l10nregistry::registry::L10nRegistry;
 use unic_langid::{langid, LanguageIdentifier};
 
 static LOCALES: &[LanguageIdentifier] = &[langid!("pl"), langid!("en-US")];
 static mut L10N_REGISTRY: Option<L10nRegistry> = None;
 
-fn fetch_sync(path: &str) -> Result<Option<String>, std::io::Error> {
-    if !std::path::Path::new(path).exists() {
-        return Ok(None);
-    }
-    Ok(Some(std::fs::read_to_string(path)?))
-}
-
 fn get_l10n_registry() -> &'static L10nRegistry {
     let reg: &mut Option<L10nRegistry> = unsafe { &mut L10N_REGISTRY };
 
     reg.get_or_insert_with(|| {
-        let mut reg = L10nRegistry::new();
+        let mut reg = L10nRegistry::default();
 
-        let main_fs = FileSource::new(
+        reg.set_lang_ids(get_app_locales().to_vec());
+
+        let main_fs = l10nregistry::tokio::file_source(
             "main".to_string(),
             get_app_locales().to_vec(),
-            "./tests/resources/{locale}/".into(),
-            fetch_sync,
+            "./tests/resources/{locale}".into(),
         );
 
         reg.register_sources(vec![main_fs]).unwrap();
@@ -38,92 +30,124 @@ fn get_app_locales() -> &'static [LanguageIdentifier] {
     LOCALES
 }
 
-type ResRc = Rc<FluentResource>;
+fn sync_localization(
+    reg: &'static L10nRegistry,
+    res_ids: Vec<String>,
+) -> SyncLocalization<L10nRegistry> {
+    SyncLocalization::with_generator(res_ids, reg.clone())
+}
 
-fn get_new_localization(reg: &'static L10nRegistry, res_ids: Vec<String>) -> Localization<ResRc> {
-    let loc = Localization::new(res_ids, move |res_ids| {
-        let locales = get_app_locales();
-        Box::new(
-            reg.generate_bundles_sync(locales, res_ids)
-                .map(|bundle| Box::new(bundle)),
+fn async_localization(
+    reg: &'static L10nRegistry,
+    res_ids: Vec<String>,
+) -> AsyncLocalization<L10nRegistry> {
+    AsyncLocalization::with_generator(res_ids, reg.clone())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup_sync_test() -> SyncLocalization<L10nRegistry> {
+        sync_localization(
+            get_l10n_registry(),
+            vec!["test.ftl".into(), "test2.ftl".into()],
         )
-    });
-    loc
-}
+    }
 
-#[test]
-fn localization_format_value_sync() {
-    let resource_ids = vec!["test.ftl".into(), "test2.ftl".into()];
+    fn setup_async_test() -> AsyncLocalization<L10nRegistry> {
+        async_localization(
+            get_l10n_registry(),
+            vec!["test.ftl".into(), "test2.ftl".into()],
+        )
+    }
 
-    let reg = get_l10n_registry();
+    #[test]
+    fn localization_format_value_sync() {
+        let loc = setup_sync_test();
 
-    let loc = get_new_localization(reg, resource_ids);
+        let value = loc.format_value_sync("hello-world", None);
+        assert_eq!(value, "Hello World [pl]");
 
-    let value = loc.format_value_sync("hello-world", None);
-    assert_eq!(value, "Hello World [pl]");
+        let value = loc.format_value_sync("missing-message", None);
+        assert_eq!(value, "missing-message");
 
-    let value = loc.format_value_sync("missing-message", None);
-    assert_eq!(value, "missing-message");
+        let value = loc.format_value_sync("hello-world-3", None);
+        assert_eq!(value, "Hello World 3 [en]");
+    }
 
-    let value = loc.format_value_sync("hello-world-3", None);
-    assert_eq!(value, "Hello World 3 [en]");
-}
+    #[test]
+    fn localization_format_values_sync() {
+        let loc = setup_sync_test();
 
-#[test]
-fn localization_format_values_sync() {
-    let resource_ids = vec!["test.ftl".into(), "test2.ftl".into()];
+        let keys = vec![
+            L10nKey {
+                id: "hello-world".to_string(),
+                args: None,
+            },
+            L10nKey {
+                id: "missing-message".to_string(),
+                args: None,
+            },
+            L10nKey {
+                id: "hello-world-3".to_string(),
+                args: None,
+            },
+        ];
+        let values = loc.format_values_sync(&keys);
+        assert_eq!(values.len(), 3);
+        assert_eq!(values[0], Some(Cow::Borrowed("Hello World [pl]")));
+        assert_eq!(values[1], None);
+        assert_eq!(values[2], Some(Cow::Borrowed("Hello World 3 [en]")));
+    }
 
-    let reg = get_l10n_registry();
+    #[tokio::test]
+    async fn localization_format_value_async() {
+        let loc = setup_async_test();
 
-    let loc = get_new_localization(reg, resource_ids);
+        let value = loc.format_value("hello-world", None).await;
+        assert_eq!(value, "Hello World [pl]");
 
-    let keys = vec![
-        L10nKey {
-            id: "hello-world".to_string(),
-            args: None,
-        },
-        L10nKey {
-            id: "missing-message".to_string(),
-            args: None,
-        },
-        L10nKey {
-            id: "hello-world-3".to_string(),
-            args: None,
-        },
-    ];
-    let values = loc.format_values_sync(&keys);
-    assert_eq!(values.len(), 3);
-    assert_eq!(values[0], Some(Cow::Borrowed("Hello World [pl]")));
-    assert_eq!(values[1], None);
-    assert_eq!(values[2], Some(Cow::Borrowed("Hello World 3 [en]")));
-}
+        let value = loc.format_value("missing-message", None).await;
+        assert_eq!(value, "missing-message");
 
-#[test]
-fn localization_format_value_async() {
-    // let resource_ids: Vec<String> = vec!["test.ftl".into(), "test2.ftl".into()];
-    // let pl = langid!("pl");
-    // let en_us = langid!("en-US");
-    // let locales = vec![&pl, &en_us];
-    //
-    // let main_fs = FileSource::new("main".to_string(), vec![pl.clone(), en_us.clone()], "./tests/resources/{locale}/".into());
-    //
-    // let mut reg = L10nRegistry::new();
-    //
-    // reg.register_sources(vec![main_fs]).unwrap();
-    //
-    // let res_ids: Vec<&Path> = resource_ids.iter().map(|res_id| res_id.as_path()).collect();
-    // let generate_messages = |_res_ids: Vec<String>| {
-    //     reg.generate_bundles(&locales, &res_ids)
-    // };
-    //
-    // let loc = Localization::new(resource_ids.clone(), generate_messages);
-    //
-    // let value = loc.format_value("hello-world", None);
-    // assert_eq!(value, "Hello World [pl]");
-    //
-    // let value = loc.format_value("missing-message", None);
-    // assert_eq!(value, "missing-message");
-    //
-    // let value = loc.format_value("hello-world-3", None);
-    // assert_eq!(value, "Hello World 3 [en]");
+        let value = loc.format_value("hello-world-3", None).await;
+        assert_eq!(value, "Hello World 3 [en]");
+    }
+
+    #[tokio::test]
+    async fn localization_format_values_async() {
+        let loc = setup_async_test();
+
+        let keys = vec![
+            L10nKey {
+                id: "hello-world".to_string(),
+                args: None,
+            },
+            L10nKey {
+                id: "missing-message".to_string(),
+                args: None,
+            },
+            L10nKey {
+                id: "hello-world-3".to_string(),
+                args: None,
+            },
+        ];
+        let values = loc.format_values(&keys).await;
+        assert_eq!(values.len(), 3);
+        assert_eq!(values[0], Some(Cow::Borrowed("Hello World [pl]")));
+        assert_eq!(values[1], None);
+        assert_eq!(values[2], Some(Cow::Borrowed("Hello World 3 [en]")));
+    }
+
+    #[tokio::test]
+    async fn localization_upgrade() {
+        let loc = setup_sync_test();
+        let value = loc.format_value_sync("hello-world", None);
+        assert_eq!(value, "Hello World [pl]");
+
+        let loc = loc.upgrade();
+        let value = loc.format_value("hello-world", None).await;
+        assert_eq!(value, "Hello World [pl]");
+    }
 }
