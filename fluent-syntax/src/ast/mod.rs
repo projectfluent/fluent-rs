f8d6cb69d6e5d283d696e7b623e4abe9ca4e89b4
Zibi Braniecki <zibi@braniecki.net>
2021-02-06 00:05:22 -0800

    Document part of AST

--- fluent-syntax/src/ast/mod.rs
+++ fluent-syntax/src/ast/mod.rs
@@ -3,8 +3,11 @@
 //! The AST of Fluent contains all nodes structures to represent a complete
 //! representation of the FTL resource.
 //!
-//! The AST is intended to preserve all semantic information and allow for round-trip
+//! The tree preserves all semantic information and allow for round-trip
 //! of a canonically written FTL resource.
+//!
+//! The root node is called [`Resource`] and contains a list of [`Entry`] nodes
+//! representing all possible entries in the Fluent Translation List.
 //!
 //! # Example
 //!
@@ -75,7 +78,7 @@
 //!
 //! ## Errors
 //!
-//! AST does preserve blocks containing invaid syntax as [`Entry::Junk`].
+//! Fluent AST preserves blocks containing invaid syntax as [`Entry::Junk`].
 //!
 //! ## White space
 //!
@@ -86,12 +89,110 @@ mod helper;
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
+/// Root node of a Fluent Translation List.
+///
+/// A [`Resource`] contains a body with a list of [`Entry`] nodes.
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = "";
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Resource<S> {
     pub body: Vec<Entry<S>>,
 }
 
+/// A top-level node representing an entry of a [`Resource`].
+///
+/// Every [`Entry`] is a standalone element and the parser is capable
+/// of recovering from errors by identifying a beginning of a next entry.
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+///
+/// key = Value
+///
+/// "#;
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Message(
+///                 ast::Message {
+///                     id: ast::Identifier {
+///                         name: "key"
+///                     },
+///                     value: Some(ast::Pattern {
+///                         elements: vec![
+///                             ast::PatternElement::TextElement {
+///                                 value: "Value"
+///                             },
+///                         ]
+///                     }),
+///                     attributes: vec![],
+///                     comment: None,
+///                 }
+///             )
+///         ]
+///     }
+/// );
+/// ```
+///
+/// # Junk Entry
+///
+/// If FTL source contains invalid FTL content, it will be preserved
+/// in form of [`Entry::Junk`] nodes.
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+///
+/// g@rb@ge En!ry
+///
+/// "#;
+///
+/// let (resource, _) = parser::parse(ftl)
+///     .expect_err("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Junk {
+///                 content: "g@rb@ge En!ry\n\n"
+///             }
+///         ]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(tag = "type"))]
@@ -104,6 +205,54 @@ pub enum Entry<S> {
     Junk { content: S },
 }
 
+/// Message node represents the most common [`Entry`] in an FTL [`Resource`].
+///
+/// A message is a localization unit with a [`Identifier`] unique within a given
+/// [`Resource`], and a value or attributes with associated [`Pattern`].
+///
+/// A message can contain a simple text value, or a compound combination of value
+/// and attributes which together can be used to localize a complex User Interface
+/// element.
+///
+/// Finally, each [`Message`] may have an associated [`Comment`].
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+///
+/// hello-world = Hello, World!
+///
+/// "#;
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Message(ast::Message {
+///                 id: ast::Identifier {
+///                     name: "hello-world"
+///                 },
+///                 value: Some(ast::Pattern {
+///                     elements: vec![
+///                         ast::PatternElement::TextElement {
+///                             value: "Hello, World!"
+///                         }
+///                     ]
+///                 }),
+///                 attributes: vec![],
+///                 comment: None,
+///             })
+///         ]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Message<S> {
@@ -113,6 +262,51 @@ pub struct Message<S> {
     pub comment: Option<Comment<S>>,
 }
 
+/// A Fluent [`Term`].
+///
+/// Terms are semantically similar to [`Message`] nodes, but
+/// they represent a separate concept in Fluent system.
+///
+/// Every term has to have a value, and the parser will
+/// report errors when term references are used in wrong positions.
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+///
+/// -brand-name = Nightly
+///
+/// "#;
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Term(ast::Term {
+///                 id: ast::Identifier {
+///                     name: "brand-name"
+///                 },
+///                 value: ast::Pattern {
+///                     elements: vec![
+///                         ast::PatternElement::TextElement {
+///                             value: "Nightly"
+///                         }
+///                     ]
+///                 },
+///                 attributes: vec![],
+///                 comment: None,
+///             })
+///         ]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Term<S> {
@@ -122,12 +316,154 @@ pub struct Term<S> {
     pub comment: Option<Comment<S>>,
 }
 
+/// Pattern contains a value of a [`Message`], [`Term`] or an [`Attribute`].
+///
+/// Each pattern is a list of [`PatternElement`] nodes representing
+/// either a simple textual value, or a combination of text literals
+/// and placeholder [`Expression`] nodes.
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+///
+/// hello-world = Hello, World!
+///
+/// welcome = Welcome, { $userName }.
+///
+/// "#;
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Message(ast::Message {
+///                 id: ast::Identifier {
+///                     name: "hello-world"
+///                 },
+///                 value: Some(ast::Pattern {
+///                     elements: vec![
+///                         ast::PatternElement::TextElement {
+///                             value: "Hello, World!"
+///                         }
+///                     ]
+///                 }),
+///                 attributes: vec![],
+///                 comment: None,
+///             }),
+///             ast::Entry::Message(ast::Message {
+///                 id: ast::Identifier {
+///                     name: "welcome"
+///                 },
+///                 value: Some(ast::Pattern {
+///                     elements: vec![
+///                         ast::PatternElement::TextElement {
+///                             value: "Welcome, "
+///                         },
+///                         ast::PatternElement::Placeable {
+///                             expression: ast::Expression::InlineExpression(
+///                                 ast::InlineExpression::VariableReference {
+///                                     id: ast::Identifier {
+///                                         name: "userName"
+///                                     }
+///                                 }
+///                             )
+///                         },
+///                         ast::PatternElement::TextElement {
+///                             value: "."
+///                         }
+///                     ]
+///                 }),
+///                 attributes: vec![],
+///                 comment: None,
+///             }),
+///         ]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Pattern<S> {
     pub elements: Vec<PatternElement<S>>,
 }
 
+/// PatternElement is an element of a [`Pattern`].
+///
+/// Each [`PatternElement`] node represents
+/// either a simple textual value, or a combination of text literals
+/// and placeholder [`Expression`] nodes.
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+///
+/// hello-world = Hello, World!
+///
+/// welcome = Welcome, { $userName }.
+///
+/// "#;
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Message(ast::Message {
+///                 id: ast::Identifier {
+///                     name: "hello-world"
+///                 },
+///                 value: Some(ast::Pattern {
+///                     elements: vec![
+///                         ast::PatternElement::TextElement {
+///                             value: "Hello, World!"
+///                         }
+///                     ]
+///                 }),
+///                 attributes: vec![],
+///                 comment: None,
+///             }),
+///             ast::Entry::Message(ast::Message {
+///                 id: ast::Identifier {
+///                     name: "welcome"
+///                 },
+///                 value: Some(ast::Pattern {
+///                     elements: vec![
+///                         ast::PatternElement::TextElement {
+///                             value: "Welcome, "
+///                         },
+///                         ast::PatternElement::Placeable {
+///                             expression: ast::Expression::InlineExpression(
+///                                 ast::InlineExpression::VariableReference {
+///                                     id: ast::Identifier {
+///                                         name: "userName"
+///                                     }
+///                                 }
+///                             )
+///                         },
+///                         ast::PatternElement::TextElement {
+///                             value: "."
+///                         }
+///                     ]
+///                 }),
+///                 attributes: vec![],
+///                 comment: None,
+///             }),
+///         ]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(tag = "type"))]
@@ -136,6 +472,69 @@ pub enum PatternElement<S> {
     Placeable { expression: Expression<S> },
 }
 
+/// Attribute represents a part of a [`Message`] or [`Term`].
+///
+/// Attributes are used to express a compound list of keyed
+/// [`Pattern`] elements on an entry.
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+///
+/// hello-world =
+///     .title = This is a title
+///     .accesskey = T
+///
+/// "#;
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Message(ast::Message {
+///                 id: ast::Identifier {
+///                     name: "hello-world"
+///                 },
+///                 value: None,
+///                 attributes: vec![
+///                     ast::Attribute {
+///                         id: ast::Identifier {
+///                             name: "title"
+///                         },
+///                         value: ast::Pattern {
+///                             elements: vec![
+///                                 ast::PatternElement::TextElement {
+///                                     value: "This is a title"
+///                                 },
+///                             ]
+///                         }
+///                     },
+///                     ast::Attribute {
+///                         id: ast::Identifier {
+///                             name: "accesskey"
+///                         },
+///                         value: ast::Pattern {
+///                             elements: vec![
+///                                 ast::PatternElement::TextElement {
+///                                     value: "T"
+///                                 },
+///                             ]
+///                         }
+///                     }
+///                 ],
+///                 comment: None,
+///             }),
+///         ]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Attribute<S> {
@@ -143,6 +542,48 @@ pub struct Attribute<S> {
     pub value: Pattern<S>,
 }
 
+/// Identifier is part of nodes such as [`Message`], [`Term`] and [`Attribute`].
+///
+/// It is used to associate a unique key with an [`Entry`] or an [`Attribute`]
+/// and in [`Expression`] nodes to refer to another entry.
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+///
+/// hello-world = Value
+///
+/// "#;
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Message(ast::Message {
+///                 id: ast::Identifier {
+///                     name: "hello-world"
+///                 },
+///                 value: Some(ast::Pattern {
+///                     elements: vec![
+///                         ast::PatternElement::TextElement {
+///                             value: "Value"
+///                         }
+///                     ]
+///                 }),
+///                 attributes: vec![],
+///                 comment: None,
+///             }),
+///         ]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Identifier<S> {
@@ -166,6 +607,44 @@ pub enum VariantKey<S> {
     NumberLiteral { value: S },
 }
 
+/// Fluent [`Comment`].
+///
+/// In Fluent, comments may be standalone, or associated with
+/// an entry such as [`Term`] or [`Message`].
+///
+/// When used as a standalone [`Entry`], comments may appear in one of
+/// three levels:
+///
+/// * Standalone comment
+/// * Group comment associated with a group of messages
+/// * Resource comment associated with the whole resource
+///
+/// # Example
+///
+/// ```
+/// use fluent_syntax::parser;
+/// use fluent_syntax::ast;
+///
+/// let ftl = r#"
+/// ## A standalone level comment
+/// "#;
+///
+/// let resource = parser::parse(ftl)
+///     .expect("Failed to parse an FTL resource.");
+///
+/// assert_eq!(
+///     resource,
+///     ast::Resource {
+///         body: vec![
+///             ast::Entry::Comment(ast::Comment {
+///                 content: vec![
+///                     "A standalone level comment"
+///                 ]
+///             })
+///         ]
+///     }
+/// );
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(from = "helper::CommentDef<S>"))]
