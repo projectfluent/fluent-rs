8009885b937faf8da0b75ed15963be16a46109e5
Mathieu Tricoire <mathieu@tricoi.re>
2022-07-27 12:25:41 -0400

    add tests

--- fluent-bundle/tests/bundle.rs
+++ fluent-bundle/tests/bundle.rs
@@ -1,14 +1,13 @@
-use fluent_bundle::{FluentBundle, FluentResource};
-use unic_langid::LanguageIdentifier;
+use fluent_bundle::{FluentArgs, FluentBundle, FluentResource};
+use std::borrow::Cow;
+use unic_langid::langid;
 
 #[test]
 fn add_resource_override() {
     let res = FluentResource::try_new("key = Value".to_string()).unwrap();
     let res2 = FluentResource::try_new("key = Value 2".to_string()).unwrap();
 
-    let en_us: LanguageIdentifier = "en-US"
-        .parse()
-        .expect("Failed to parse a language identifier");
+    let en_us = langid!("en-US");
     let mut bundle = FluentBundle::new(vec![en_us]);
 
     bundle.add_resource(&res).expect("Failed to add a resource");
@@ -19,19 +18,75 @@ fn add_resource_override() {
 
     let value = bundle
         .get_message("key")
-        .expect("Failed to retireve a message")
+        .expect("Failed to retrieve a message")
         .value()
-        .expect("Failed to retireve a value of a message");
+        .expect("Failed to retrieve a value of a message");
     assert_eq!(bundle.format_pattern(value, None, &mut errors), "Value");
 
     bundle.add_resource_overriding(&res2);
 
     let value = bundle
         .get_message("key")
-        .expect("Failed to retireve a message")
+        .expect("Failed to retrieve a message")
         .value()
-        .expect("Failed to retireve a value of a message");
+        .expect("Failed to retrieve a value of a message");
     assert_eq!(bundle.format_pattern(value, None, &mut errors), "Value 2");
 
     assert!(errors.is_empty());
+}
+
+#[test]
+fn borrowed_plain_message() {
+    let res = FluentResource::try_new("key = Value".to_string()).unwrap();
+    let en_us = langid!("en-US");
+
+    let mut bundle = FluentBundle::new(vec![en_us]);
+    bundle.add_resource(&res).expect("Failed to add a resource");
+
+    let mut errors = vec![];
+
+    let formatted_pattern = {
+        let value = bundle
+            .get_message("key")
+            .expect("Failed to retrieve a message")
+            .value()
+            .expect("Failed to retrieve a value of a message");
+
+        bundle.format_pattern(value, None, &mut errors)
+    };
+
+    fn is_borrowed(cow: Cow<'_, str>) -> bool {
+        match cow {
+            Cow::Borrowed(_) => true,
+            _ => false,
+        }
+    }
+    assert_eq!(formatted_pattern, "Value");
+    assert!(is_borrowed(formatted_pattern));
+}
+
+#[test]
+fn arguments_outlive_formatted_pattern() {
+    let res = FluentResource::try_new("key = { $variable }".to_string()).unwrap();
+    let en_us = langid!("en-US");
+
+    let mut bundle = FluentBundle::new(vec![en_us]);
+    bundle.add_resource(&res).expect("Failed to add a resource");
+
+    let mut errors = vec![];
+
+    let formatted_pattern = {
+        let value = bundle
+            .get_message("key")
+            .expect("Failed to retrieve a message")
+            .value()
+            .expect("Failed to retrieve a value of a message");
+
+        let mut args = FluentArgs::new();
+        args.set("variable", "Variable");
+
+        bundle.format_pattern(value, Some(&args), &mut errors)
+    };
+
+    assert_eq!(formatted_pattern, "Variable");
 }
