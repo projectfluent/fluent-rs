9fac18a1c46c10de27d6818c36c70f415ed4ecb3
Greg Tatum <tatum.creative@gmail.com>
2022-10-26 11:33:10 -0500

    Document resolvers in the fluent-bundle

--- fluent-bundle/src/resolver/mod.rs
+++ fluent-bundle/src/resolver/mod.rs
@@ -1,3 +1,8 @@
+//! The `resolver` module contains the definitions and implementations for the internal
+//! `ResolveValue` and `WriteValue` traits. The former converts AST nodes to a
+//! [`FluentValue`], and the latter converts them to a string that is written to an
+//! implementor of the [`std::fmt::Write`] trait.
+
 pub mod errors;
 mod expression;
 mod inline_expression;
@@ -14,8 +19,9 @@ use crate::memoizer::MemoizerKind;
 use crate::resource::FluentResource;
 use crate::types::FluentValue;
 
-// Converts an AST node to a `FluentValue`.
+/// Resolves an AST node to a [`FluentValue`].
 pub(crate) trait ResolveValue<'bundle> {
+    /// Resolves an AST node to a [`FluentValue`].
     fn resolve<'ast, 'args, 'errors, R, M>(
         &'ast self,
         scope: &mut Scope<'bundle, 'ast, 'args, 'errors, R, M>,
@@ -25,7 +31,9 @@ pub(crate) trait ResolveValue<'bundle> {
         M: MemoizerKind;
 }
 
+/// Resolves an AST node to a string that is written to source `W`.
 pub(crate) trait WriteValue<'bundle> {
+    /// Resolves an AST node to a string that is written to source `W`.
     fn write<'ast, 'args, 'errors, W, R, M>(
         &'ast self,
         w: &mut W,
@@ -36,6 +44,8 @@ pub(crate) trait WriteValue<'bundle> {
         R: Borrow<FluentResource>,
         M: MemoizerKind;
 
+    /// Writes error information to `W`. This can be used to add FTL errors inline
+    /// to a message.
     fn write_error<W>(&self, _w: &mut W) -> fmt::Result
     where
         W: fmt::Write;
