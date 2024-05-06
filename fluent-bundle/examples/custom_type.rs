f7745968cc53e2dbf5e8db19809a8c5111e90d57
Bruce Mitchener <bruce.mitchener@gmail.com>
2024-05-06 09:00:30 +0700

    refactor: Derive `Default` for enums
    As of Rust 1.62, enums can use `#[derive(Default)]`.
    
    See <https://github.com/rust-lang/rust/pull/94457>.

--- fluent-bundle/examples/custom_type.rs
+++ fluent-bundle/examples/custom_type.rs
@@ -21,19 +21,14 @@ use fluent_bundle::{FluentArgs, FluentBundle, FluentResource, FluentValue};
 //  - timeStyle
 //
 // with an enum of allowed values.
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+#[derive(Debug, Default, PartialEq, Eq, Clone, Hash)]
 enum DateTimeStyleValue {
     Full,
     Long,
     Medium,
     Short,
+    #[default]
     None,
-}
-
-impl std::default::Default for DateTimeStyleValue {
-    fn default() -> Self {
-        Self::None
-    }
 }
 
 // This is just a helper to make it easier to convert
